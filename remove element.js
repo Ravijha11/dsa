68ebad021d3ebff6199ca0c8459292430840ce22
RAVINDRA JHA <ravijha110903@gmail.com>
2024-04-09 14:50:56 +0530

    rotate array

---
+++ remove element.js
@@ -0,0 +1,37 @@
+
+// // let i=0
+// // function ravi(arr,k){
+// //   for  (let j =1;j<k;j++){
+// //     if(arr[i]!=arr[j]){
+// //       arr[i+1] += arr[j]
+// //     }
+// //   }
+// //   return i+1;
+// // }
+// // console.log(ravi([1,1,2,2,3,4,4,4,7,8,9,9,9],13))
+// let array= [1,1,2,3,4,4,4,5,5,5,6,6];
+// let ravi = new Set(array);
+// console.log(ravi)
+
+
+
+// < --------------------------------------------remove element from duplicate array-------------------------------------->
+
+
+// function ravi(arr){
+//   let k = arr.length;
+  
+//   let ravi2 =[];
+//   let j=0;
+//   for (let i=1;i<=k;i++){
+//     if(arr[j]!=arr[i]){
+//       ravi2.push(arr[j])
+      
+//     }
+//     j++
+//   }
+//   return ravi2
+// }
+// console.log(ravi([1,2,2,6,6,8,8,9,9,],))
+
+function second
---
+++ sorted.js
@@ -0,0 +1,19 @@
+function so(arr, k){
+
+  for(let i=1;i<k;i++){
+    if(arr[i-1]<arr[i]){
+      
+    }
+    else{
+      return false;
+      
+    }
+  
+    
+  }
+  return true;
+ 
+  
+}
+
+console.log(so([1,2,3,4,5,6,7,2,9]))
