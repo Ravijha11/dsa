1fd0dd01ca6a154aa71178f8897fc0ad8446455c
RAVINDRA JHA <ravijha110903@gmail.com>
2024-04-13 14:46:36 +0530

    movezerotillend

---
+++ movezero.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  n = nums.length;
+  let  temp = [];
+  for(let i =0;i<n;i++){
+      if(nums[i]!=0){
+          temp.push(nums[i])
+      }
+
+  }
+  for(let i =0;i<temp.length;i++){
+      nums[i] = temp[i]
+  }
+  for(let i = temp.length;i<n;i++){
+      nums[i] = 0;
+  }
+  
+};
